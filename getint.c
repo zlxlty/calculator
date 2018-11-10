66657e61a7feda698e463b6b26d677dab1f739e1
SkyLu <zlxlty>
2018-11-10 19:05:31 +0800

    added getint func

---
+++ getint.c
@@ -0,0 +1,49 @@
+#include "calc.h"
+#include <stdio.h>
+#include <ctype.h>
+#define SIZE 10
+
+extern int getch(void);
+extern void ungetch(int);
+
+int getint(int *pn);
+
+int main(int argc, char const *argv[]) {
+
+    int n, array[SIZE];
+
+    for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++)
+        ;
+
+    for (n = 0; n < SIZE; n++)
+        printf("%d\n", array[n]);
+
+    return 0;
+}
+
+int getint(int *pn)
+{
+    int c, sign;
+
+    while (isspace(c = getch()))
+        ;
+
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-')
+    {
+        ungetch(c);
+        return 0;
+    }
+
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-')
+        c = getch();
+
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+
+    *pn *= sign;
+    if (c != EOF)
+        ungetch(c);
+
+    return c;
+}
